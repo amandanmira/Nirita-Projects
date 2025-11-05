83cb9dfc29ac0a22a8991917553730a6c280ca18
GilangDafa10 <gilangdafa64@gmail.com>
2025-11-06 01:48:56 +0700

    Menambahkan jam operasional di footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -3,8 +3,8 @@ import Logo from "../Assets/Logo Nirita Rentals.png";
 </script>
 
 <template>
-    <footer class="bg-[#0f172a] text-gray-300 py-10 md:px-16 lg:px-16">
-        <div class="max-w-7xl mx-auto px-6 grid md:grid-cols-3 gap-8">
+    <footer class="bg-[#0f172a] text-gray-300 py-10 md:px-14">
+        <div class="max-w-7xl mx-auto px-6 grid md:grid-cols-4 gap-8">
             <!-- Logo & Description -->
             <div>
                 <div class="flex items-center space-x-3 mb-3">
@@ -24,8 +24,8 @@ import Logo from "../Assets/Logo Nirita Rentals.png";
             <div>
                 <h3 class="text-white font-semibold mb-3">Kontak Kami</h3>
                 <p class="text-sm leading-relaxed">
-                    Jl. Nuri 2B, Perumahan Gumpang Agung 1,<br />
-                    Kec. Kartasura, Kabupaten Sukoharjo
+                    Jl. Nuri 2B, Perumahan Gumpang Agung 1, Kec. Kartasura,
+                    Kabupaten Sukoharjo
                 </p>
                 <p class="mt-2 text-sm">
                     WhatsApp:
@@ -43,10 +43,22 @@ import Logo from "../Assets/Logo Nirita Rentals.png";
                 </p>
             </div>
 
+            <!-- Jam Operasional -->
+            <div>
+                <h3 class="text-white font-semibold mb-3">Jam Operasional</h3>
+                <p class="mt-2 text-sm">
+                    Senin - Minggu: 08.00 - 12.00 WIB & 17.00 - 24.00 WIB
+                    (Offline)
+                </p>
+                <p class="mt-2 text-sm">
+                    Senin - Minggu: 05.00 - 22.30 WIB (Via Wa)
+                </p>
+            </div>
+
             <!-- Navigation -->
             <div>
                 <h3 class="text-white font-semibold mb-3">Navigasi</h3>
-                <ul class="space-y-1 text-sm">
+                <ul class="space-y-1 text-sm flex gap-3 md:flex-col md:gap-0">
                     <li>
                         <RouterLink to="#" class="hover:text-blue-400"
                             >Beranda</RouterLink
